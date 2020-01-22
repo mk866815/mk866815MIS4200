8f872af6be7640231fabe475cae57a913b1a67e2
Klimack <mk866815@ohio.edu>
2020-01-22 11:16:08 -0500

    Add project files.

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(mk866815MIS4200.Startup))]
+namespace mk866815MIS4200
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
