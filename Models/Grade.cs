bdba610b4e65aa988277ba6b3f34080eae785612
Morgan Klimack <mk866815@ohio.edu>
2020-02-07 11:37:00 -0500

    models grade and student

---
+++ Models/Grade.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace mk866815MIS4200.Models
+{
+    public class Grade
+    {
+        public int gradeID { get; set; }
+        public  string grade { get; set; }
+
+        public DateTime gradeDate { get; set; }
+
+        public int studentID { get; set; }
+        public virtual Student Student { get; set; }
+
+
+    }
+}
---
+++ Models/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace mk866815MIS4200.Models
+{
+    public class Student
+    {
+        public int studentID { get; set; } 
+        public  string firstName { get; set; }
+        public  string lastName { get; set; }
+        public  string email { get; set;  }
+        public string studentGrade { get; set; }
+
+        public ICollection<Grade> Grade { get; set; }
+    }
+}
